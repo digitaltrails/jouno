c0b978b303eaffd441ceea792fc7e1927987fb50
digitaltrails <michael@actrix.gen.nz>
2021-10-10 22:21:03 +1300

    First tidy up

--- jnotify.py
+++ jnotify.py
@@ -1,9 +1,8 @@
 import select
+from typing import Mapping, Any
 
 import dbus
 from systemd import journal
-# This is a sample Python script.
-from notify2 import Notification
 
 import configparser
 
@@ -28,20 +27,21 @@ class NotifyFreeDesktop:
                                      extra_hints,
                                      timeout)
 
+
 class JournalWatcher:
 
     def __init__(self, config: configparser.ConfigParser):
         self.config = config
 
-    def determine_app_name(self, entry):
+    def determine_app_name(self, journal_entry: Mapping[str, Any]):
         app_name_info = ''
         sep = ''
         for key, prefix in {'SYSLOG_IDENTIFIER': '', '_PID': 'PID ', '_CMDLINE': '', '_EXE': '', '_COMM': '',
                             '_KERNEL_SUBSYSTEM': 'kernel ',
                             }.items():
-            print(key, entry[key] if key in entry else False)
-            if key in entry:
-                value = str(entry[key])
+            print(key, journal_entry[key] if key in journal_entry else False)
+            if key in journal_entry:
+                value = str(journal_entry[key])
                 if app_name_info.find(value) < 0:
                     app_name_info += sep + prefix + value
                     sep = '; '
@@ -49,26 +49,28 @@ class JournalWatcher:
             app_name_info = 'unknown'
         return app_name_info
 
-
-    def determine_summary(self, entry, burst_count: int = 0):
-        realtime = entry['__REALTIME_TIMESTAMP']
-        transport = f" {entry['_TRANSPORT']}:" if '_TRANSPORT' in entry else ''
+    def determine_summary(self, journal_entry: Mapping[str, Any], burst_count: int = 0):
+        realtime = journal_entry['__REALTIME_TIMESTAMP']
+        transport = f" {journal_entry['_TRANSPORT']}:" if '_TRANSPORT' in journal_entry else ''
         print(realtime)
         if burst_count > 1:
             summary = f"{realtime:%H:%M:%S}:{transport} Burst of {burst_count} messages, first was..."
         else:
-            summary = f"{realtime:%H:%M:%S}:{transport} {entry['MESSAGE'] if 'SYSLOG_IDENTIFIER' not in entry else entry['SYSLOG_IDENTIFIER']}"
+            if 'SYSLOG_IDENTIFIER' not in journal_entry:
+                text = journal_entry['SYSLOG_IDENTIFIER']
+            else:
+                text = journal_entry['MESSAGE']
+            summary = f"{realtime:%H:%M:%S}: {transport} {text} "
         print("summary=", summary)
         return summary
 
-
-    def determine_message(self, entry):
-        message = entry['MESSAGE']
+    def determine_message(self, journal_entry: Mapping[str, Any]) -> str:
+        message = journal_entry['MESSAGE']
         print(message)
         return f"{message}"
 
-    def is_notable(self, entry):
-        message = entry['MESSAGE']
+    def is_notable(self, journal_entry: Mapping[str, Any]):
+        message = journal_entry['MESSAGE']
         if message != "":
             for ignore_key, ignore_spec in self.config['IGNORE'].items():
                 if ignore_spec in message:
@@ -76,7 +78,7 @@ class JournalWatcher:
                     return False
         return True
 
-    def watch(self):
+    def watch_journal(self):
         notify = NotifyFreeDesktop()
 
         journal_reader = journal.Reader()
@@ -91,45 +93,21 @@ class JournalWatcher:
         journal_reader_poll.register(journal_reader, journal_reader.get_events())
         journal_reader.add_match()
         while True:
-            burst_count = 0;
+            burst_count = 0
             first_notable = None
             while journal_reader_poll.poll(2000):
                 if journal_reader.process() == journal.APPEND:
-                    for entry in journal_reader:
+                    for journal_entry in journal_reader:
                         burst_count += 1
-                        if self.is_notable(entry):
+                        if self.is_notable(journal_entry):
                             if first_notable is None:
-                                first_notable = entry
+                                first_notable = journal_entry
                         if first_notable:
-                            print(f"count={burst_count} {entry['MESSAGE']}")
+                            print(f"count={burst_count} {journal_entry['MESSAGE']}")
             if first_notable is not None:
                 notify.notify_desktop(app_name=self.determine_app_name(first_notable),
                                       summary=self.determine_summary(first_notable, burst_count=burst_count),
                                       message=self.determine_message(first_notable))
-
-
-# def looping():
-#     notify = NotifyFreeDesktop()
-#     j = journal.Reader()
-#     j.seek_tail()
-#     j.get_previous()
-#     while True:
-#         j_state = j.wait(-1)
-#         if j_state == journal.APPEND:
-#             count = 1
-#             last_app_name = None
-#             for entry in j:
-#                 print(entry)
-#                 if count == 1:
-#                     last_app_name = determine_app_name(entry)
-#                     notify.notify_desktop(app_name=last_app_name,
-#                                           summary=determine_summary(entry),
-#                                           message=entry['MESSAGE'])
-#                 count += 1
-#             if count > 1:
-#                 notify.notify_desktop(app_name=last_app_name,
-#                                       summary="End of a burst of messages",
-#                                       message=f"The burst had {count} messages in total.")
 
 
 def main():
@@ -139,8 +117,8 @@ def main():
                         'xyzzy': 'xyzzy',
                         'self_caused': 'NotificationPopup.',
                         'qt_kde_binding_loop': 'Binding loop detected for property'}
-    j_watcher = JournalWatcher(config)
-    j_watcher.watch()
+    journal_watcher = JournalWatcher(config)
+    journal_watcher.watch_journal()
 
 
 # Press the green button in the gutter to run the script.
