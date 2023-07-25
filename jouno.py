e163277280e48268a3cdb5c2aba47e965f455720
digitaltrails <michael@actrix.gen.nz>
2023-07-25 14:29:16 +1200

    Disable selection in the filter table - makes no sense and is confusing.

--- jouno.py
+++ jouno.py
@@ -1664,7 +1664,7 @@ class FilterTableView(QTableView):
         self.verticalHeader().setDragDropMode(QAbstractItemView.InternalMove)
         self.setDragDropOverwriteMode(True)
         self.resizeColumnsToContents()
-        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.setSelectionMode(QAbstractItemView.NoSelection)
         self.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
         self.horizontalHeader().setDefaultAlignment(Qt.AlignLeft)
